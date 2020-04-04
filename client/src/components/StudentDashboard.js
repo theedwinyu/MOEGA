be7c0429568b1e072dfa204a1759c4482e89e95f
JoonRhee <sj78sj78@gmail.com>
2020-04-05 07:13:07 +0900

    camera and socket

--- client/src/components/StudentDashboard.js
+++ client/src/components/StudentDashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {withRouter} from "react-router-dom"
+import io from 'socket.io-client'
+import ml5 from 'ml5'
 
 class StudentDashboard extends Component{
 
@@ -7,11 +9,66 @@ class StudentDashboard extends Component{
         super(props);
     }
 
+    async componentDidMount(){
+        const socket = io("http://localhost:5000/");
+        socket.emit("joinroom",this.props.location.state.values.roomID)
+        socket.emit("joinnotif",this.props.location.state.values.roomID,this.props.location.state.values.name)
+        const video = document.getElementById('video')
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true })
+              .then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+              })
+              .catch(function (err0r) {
+                console.log(err0r);
+            });
+        }
+
+        const poseNet = ml5.poseNet(video,"single", modelLoaded);
+        function modelLoaded() {
+            console.log('Model Loaded!');
+        }
+        poseNet.on('pose', (poses) => {
+            if(poses){
+                let pose = poses[0].pose;
+                checkHandsUp(pose,this.props.location.state.values.roomID,this.props.location.state.values.name)
+            }
+        
+        });
+        
+        function checkHandsUp(pose,roomID,name) {
+            let nosey = pose.nose.y
+            let nosec = pose.nose.confidence
+        
+            let leftwristy = pose.leftWrist.y
+            let leftwristc = pose.leftWrist.confidence
+        
+            let rightwristy = pose.rightWrist.y
+            let rightwristc = pose.rightWrist.confidence
+        
+            if(nosec < 0.50 || (rightwristc < 0.50 && leftwristc < 0.50)){
+                console.log("bad c")
+                return
+            }
+        
+            if(leftwristy < nosey || rightwristy < nosey){
+                socket.emit("hand",roomID,name)
+                console.log("raised")
+                return
+            }
+        
+            console.log('waiting')
+        }
+
+    }
+
     render(){
         console.log(this.props.location.state);
         return(
 
             <div>
+                <video id="video" width="640" height="480" autoPlay></video>
                 <h1>Room {this.props.location.state.values.roomID}</h1>
                 <h1>Welcome {this.props.location.state.values.name}</h1>
             </div>
